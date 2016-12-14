c40d594132f8b2995fdb04b7b1b7863ea52603ed
Steven Kronmueller <kronmueller.s@gmail.com>
2016-12-14 11:10:31 -0500

    Add files via upload

---
+++ FinalExam-master/Assets/Scripts/DestroyBall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collisions : MonoBehaviour
+{
+
+    public AudioSource catbat;
+
+    void OnCollisionEnter(Collision collider)
+    {
+        if (collider.gameObject.name.Equals("Bat"))
+            catbat.Play();
+
+        GameObject.Destroy(gameObject);
+    }
+}
