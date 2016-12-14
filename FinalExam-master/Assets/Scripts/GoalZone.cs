f559428444274e50bd1f68947b62d99e833d81e4
Steven Kronmueller <kronmueller.s@gmail.com>
2016-12-14 11:11:19 -0500

    Add files via upload

---
+++ FinalExam-master/Assets/Scripts/GoalZone.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GoalZone : MonoBehaviour
+{
+
+    public AudioSource goalAudio;
+
+    // Reference to all the particle systems we want to fire when a goal is scored.
+    public List<ParticleSystem> goalParticleSystems;
+
+    /* If you're working with a triggered collision (intersection),
+   * this is called when the triggered collision starts. */
+    void OnTriggerStay(Collider collider)
+    {
+        goalAudio.Play();
+
+        Destroy(collider.gameObject);
+    }
+
+}
+
+ 
---
+++ FinalExam-master/Assets/Scripts/RotateAround.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour
+{
+
+    public float rotationSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // point around which we rotate (world coordinates)
+        // axis is the vector we rotate around.
+        transform.RotateAround(transform.parent.position, Vector3.up, rotationSpeed * Time.deltaTime);
+
+        // Rotate around an axis; specify the tilt of the axis, and the amount to rotate/frame.
+        transform.Rotate(new Vector3(10, 20, 5), rotationSpeed * Time.deltaTime);
+
+        /*foreach(Transform child in transform.parent) {
+          Debug.Log("Where my child's at? " + child);
+        }*/
+    }
+}
